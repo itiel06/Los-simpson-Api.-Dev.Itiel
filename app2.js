ecbdac873305df592be14046eee519914b027c95
Itiel06 <itiel.martinez06@gmail.com>
2025-10-08 18:32:43 -0400

    v2.1.0

--- app2.js
+++ app2.js
@@ -3,29 +3,39 @@ const baseInput = document.getElementById('base')
 const limiteInput = document.getElementById('limite')
 const boton = document.getElementById('button')
 const contenedor = document.getElementById('contenedor')
-
+const contenedore = document.getElementById('contenedore')
 
 const getapi = async (base = 1, limite = 2) => {
-    
-    contenedor.innerHTML= ''
+    try {
+         contenedor.classList.remove('contenedorerr')
 
-    for ( base; base  <= limite; base++) {
 
-        const response = await fetch(`https://thesimpsonsapi.com/api/characters/${base}`)
-        const data = await response.json();
+        contenedor.innerHTML = ''
 
-        const { id, age, gender, name, first_appearance_ep: { image_path }, portrait_path,status } = data;
+        for (base; base <= limite; base++) {
 
-        contenedor.innerHTML += `
-<div class='contenedor2'>
-<h3 class='textoid'>${id}</h3>
-<h2 class='nombre'>${name}</h2>
-<p class='edad'> Age:${age}</P>
-<P class='genero'> Gender: ${gender}</p>
-<p class='status1'> Status: ${status}</p>
-<img src='https://cdn.thesimpsonsapi.com/200${portrait_path}' >
-</div>
-`
+            const response = await fetch(`https://thesimpsonsapi.com/api/characters/${base}`)
+            const data = await response.json();
+
+            const { id, age, gender, name, first_appearance_ep: { image_path }, portrait_path, status } = data;
+
+            contenedor.innerHTML += `
+            <div class='contenedor2'>
+            <h3 class='textoid'>${id}</h3>
+            <h2 class='nombre'>${name}</h2>
+            <p class='edad'> Age:${age}</P>
+            <P class='genero'> Gender: ${gender}</p>
+            <p class='status1'> Status: ${status}</p>
+            <img src='https://cdn.thesimpsonsapi.com/200${portrait_path}' >
+            </div>
+            `
+        
+
+        }
+
+    } catch (error) { 
+        contenedor.textContent = error
+        contenedor.classList.add('contenedorerr')
     }
 }
 
@@ -34,9 +44,9 @@ const getapi = async (base = 1, limite = 2) => {
 
 
 
-boton.addEventListener('click', ()=>{
+boton.addEventListener('click', () => {
 
-getapi(baseInput.value, limiteInput.value)
+    getapi(baseInput.value, limiteInput.value)
 
 
 })
